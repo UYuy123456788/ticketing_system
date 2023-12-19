1d7dc395d1f42f4bf13e752567326d30abdb1081
UYuy123456788 <154337460+UYuy123456788@users.noreply.github.com>
2023-12-19 21:08:44 +0800

    Add files via upload

---
+++ ticketing_system/database/helpdesk_system.sql
@@ -0,0 +1,183 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 19, 2023 at 02:05 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `helpdesk_system`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hd_departments`
+--
+
+CREATE TABLE `hd_departments` (
+  `id` int(11) NOT NULL,
+  `name` varchar(250) NOT NULL,
+  `status` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `hd_departments`
+--
+
+INSERT INTO `hd_departments` (`id`, `name`, `status`) VALUES
+(1, 'Technical', 1),
+(2, 'Testing', 1),
+(3, 'Automation', 1),
+(4, 'Design', 1),
+(5, 'Programming', 1),
+(7, 'Security', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hd_tickets`
+--
+
+CREATE TABLE `hd_tickets` (
+  `id` int(11) NOT NULL,
+  `uniqid` varchar(20) NOT NULL,
+  `user` int(11) NOT NULL,
+  `title` varchar(250) NOT NULL,
+  `init_msg` text NOT NULL,
+  `department` int(11) NOT NULL,
+  `date` varchar(250) NOT NULL,
+  `last_reply` int(11) NOT NULL,
+  `user_read` int(11) NOT NULL,
+  `admin_read` int(11) NOT NULL,
+  `resolved` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hd_ticket_replies`
+--
+
+CREATE TABLE `hd_ticket_replies` (
+  `id` int(11) NOT NULL,
+  `user` int(11) NOT NULL,
+  `text` text NOT NULL,
+  `ticket_id` text NOT NULL,
+  `date` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `hd_ticket_replies`
+--
+
+INSERT INTO `hd_ticket_replies` (`id`, `user`, `text`, `ticket_id`, `date`) VALUES
+(1, 1, 'This is fixed', '1', '1634829030'),
+(2, 1, 'Please check it.', '1', '1634829129'),
+(3, 1, 'The issue is fixed, you can check at your end. Thanks', '2', '1634829404'),
+(4, 2, 'fixed', '2', '1635515403'),
+(5, 2, 'this is fixed!', '4', '1635517083'),
+(6, 1, 'I am looking into this', '5', '1635519340'),
+(7, 2, 'ewtewt', '6', '1635519418'),
+(8, 1, 'Done', '7', '1702556538'),
+(9, 1, 'wait for 1 week', '10', '1702733086');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hd_users`
+--
+
+CREATE TABLE `hd_users` (
+  `id` int(11) NOT NULL,
+  `email` varchar(250) NOT NULL,
+  `password` varchar(250) NOT NULL,
+  `create_date` datetime NOT NULL DEFAULT current_timestamp(),
+  `name` varchar(250) NOT NULL,
+  `user_type` enum('admin','user') NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `hd_users`
+--
+
+INSERT INTO `hd_users` (`id`, `email`, `password`, `create_date`, `name`, `user_type`, `status`) VALUES
+(1, 'admin', '202cb962ac59075b964b07152d234b70', '2021-10-25 23:24:33', 'Admin Stephen', 'admin', 1),
+(2, 'pendong@gmail.com', '202cb962ac59075b964b07152d234b70', '2021-10-25 23:24:46', 'Stephen', 'user', 1),
+(43, 'fakersenpai014@gmail.com', '$2y$10$O/uBBsbl7tZou/Aq1xL/SOkLGsKUNqIGvTU83SsCwqF83ymmTDlOe', '2023-12-16 14:26:56', 'mike', 'user', 1),
+(44, 'mike@gmail.com', '$2y$10$8Ceq6jsyPCMOd2r9X9tLv.tKU9m/CR40MEfip0kCMHKW7lGqdWJ2i', '2023-12-16 14:27:40', 'Mike', 'user', 1),
+(45, 'asd@gmail.com', '$2y$10$8qQ2Fx6Sfze9IYl7Qw0zje9uuGvLICGfMcjPFX.4iANDGAf.tp.3y', '2023-12-16 14:28:54', 'asd', 'user', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `hd_departments`
+--
+ALTER TABLE `hd_departments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `hd_tickets`
+--
+ALTER TABLE `hd_tickets`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `hd_ticket_replies`
+--
+ALTER TABLE `hd_ticket_replies`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `hd_users`
+--
+ALTER TABLE `hd_users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `hd_departments`
+--
+ALTER TABLE `hd_departments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `hd_tickets`
+--
+ALTER TABLE `hd_tickets`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `hd_ticket_replies`
+--
+ALTER TABLE `hd_ticket_replies`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `hd_users`
+--
+ALTER TABLE `hd_users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
